e50bad00757fac2946788b93c98447b8152d173e
Prashant Saxena <prashantsaxena0702@gmail.com>
2024-05-06 14:25:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using SortingAlgo;
+
+
+#region Quick Sort Algoritham
+//Console.WriteLine("Hello, World!");
+
+//QuickSort quickSort = new QuickSort();
+
+//int[] arr = { 67, 12, 95, 56, 85, 1, 100, 23, 60, 9 };
+//int n = 10, i;
+//Console.WriteLine("Quick Sort");
+//Console.Write("Initial array is: ");
+//for (i = 0; i < n; i++)
+//{
+//    Console.Write(arr[i] + " ");
+//}
+//quickSort.quickSort(arr, 0, 9);
+//Console.WriteLine("\n");
+//Console.Write("Sorted Array is: ");
+//         for (i = 0; i < n; i++)
+//{
+//    Console.Write(arr[i] + " ");
+//}
+
+#endregion
+#region Suduko Algoritham
+//GFG gFG = new GFG();
+//int[,] grid = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+//                         { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+//                         { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+//                         { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+//                         { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+//                         { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+//                         { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+//                         { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+//                         { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+
+//if (gFG.solveSudoku(grid, 0, 0))
+//    gFG.print(grid);
+//else
+//    Console.WriteLine("No Solution exists");
+
+#endregion
+#region Rat in Maze
+RatMaze rat = new RatMaze();
+ int N;
+int[,] maze = { { 1, 0, 0, 0 },
+                    { 1, 1, 0, 1 },
+                    { 0, 1, 0, 0 },
+                    { 1, 1, 1, 1 } };
+
+N = maze.GetLength(0);
+rat.solveMaze(maze);
+#endregion
+
+
+
+                             
---
+++ QuickSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgo
+{
+    public class QuickSort
+    {
+        public QuickSort()
+        {
+
+        }
+         public int Partition(int[] arr, int left, int right)
+        {
+            int pivot;
+            pivot = arr[left];
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+                if (left < right)
+                {
+                    int temp = arr[right];
+                    arr[right] = arr[left];
+                    arr[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+
+        }
+
+         public void quickSort(int[] arr, int left, int right)
+        {
+            int pivot;
+            if (left < right)
+            {
+                pivot = Partition(arr, left, right);
+                if (pivot > 1)
+                {
+                    quickSort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    quickSort(arr, pivot + 1, right);
+                }
+            }
+        }
+    }
+}
